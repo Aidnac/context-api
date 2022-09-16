c675a7e51e447c2953a548aa356d64a36b58f847
Can-A <calex6658@gmail.com>
2022-09-16 22:15:34 +0300

    Reusable component

--- src/index.js
+++ src/index.js
@@ -4,28 +4,36 @@ import SingleComment from './SingleComment'
 import profile1 from './image/pic1.jfif'
 import profile2 from './image/pic2.jfif'
 import profile3 from './image/pic3.jfif'
+import UserCard from "./UserCard";
 
 const App = () =>{
     return(
-        <div className='ol comments'>
-            <SingleComment 
-                name='Alex' 
-                date='Today at 5:00 PM'
-                text='it is amazing'
-                picture={profile1}
-            />
-            <SingleComment 
-                name='Sarah' 
-                date='Today at 6:00 PM'
-                text='Great job'
-                picture={profile2}
-            />
-            <SingleComment 
-                name='Jack' 
-                date='Today at 7:00 PM'
-                text='Thanks'
-                picture={profile3}
-            />
+        <div className='ui comments'>
+            <UserCard>
+                <SingleComment 
+                    name='Alex' 
+                    date='Today at 5:00 PM'
+                    text='it is amazing'
+                    picture={profile1}
+                />
+            </UserCard>
+            
+            <UserCard>
+                <SingleComment 
+                    name='Sarah' 
+                    date='Today at 6:00 PM'
+                    text='Great job'
+                    picture={profile2}
+                />
+            </UserCard>
+            <UserCard>
+                <SingleComment 
+                    name='Jack' 
+                    date='Today at 7:00 PM'
+                    text='Thanks'
+                    picture={profile3}
+                />
+            </UserCard>
         </div>
     )
 }
