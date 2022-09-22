9ef498be99ff24a0be853f20c6662c3814bbbe38
Can-A <calex6658@gmail.com>
2022-09-22 18:02:49 +0300

    Create multiple contexts

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import Navbar from "./components/Navbar";
 import TodoList from "./components/TodoList";
 import './index.css'
 import ThemeContextProvider from "./contexts/ThemeContext";
-
+import AuthContextProvider from "./contexts/AuthContext";
 function App(){
     return(
         <div className="App">
             <div className="ui raised very padded text container segment">
-                <ThemeContextProvider>
-                    <Navbar/>
-                    <TodoList/>
-                </ThemeContextProvider>
+                <AuthContextProvider>
+                    <ThemeContextProvider>
+                        <Navbar/>
+                        <TodoList/>
+                    </ThemeContextProvider>
+                </AuthContextProvider>
             </div>
         </div>
     )
